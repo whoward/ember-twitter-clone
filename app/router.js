a6c21ee8c75b290c6a9ec744900e3391388cdd33
William Howard <whoward.tke@gmail.com>
2016-02-12 03:19:15 +0000

    initial commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('feed', {path: '/'});
+  this.route('my_tweets', {path: '/profile'});
 });
 
 export default Router;
