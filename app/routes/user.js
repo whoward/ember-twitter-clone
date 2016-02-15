b45013930a06b17cc954c0af8db0033c29b9ac0e
William Howard <whoward.tke@gmail.com>
2016-02-15 06:03:27 +0000

    make the user page work

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return params.handle;
+    return {
+      user: this.store.findRecord('user', params.handle),
+      tweets: this.store.query('tweet', {user: params.handle})
+    };
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,23 @@
+
 export default function() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
-  this.get('/tweets');
-  this.get('/users/:id');
+  let findUserByHandle = function(schema, handle) {
+    return schema.user.where({handle: handle})[0];
+  };
+
+  this.get('/tweets', function(schema, request) {
+    if(request.queryParams.user) {
+      let user = findUserByHandle(schema, request.queryParams.user);
+      return schema.tweet.where({authorId: user.id});
+    } else {
+      return schema.tweet.all();
+    }
+  });
+  
+  this.get('/users/:handle', function(schema, request) {
+    return findUserByHandle(schema, request.params.handle);
+  });
 }
