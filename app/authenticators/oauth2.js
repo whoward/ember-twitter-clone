7b6753dc4b910152b0660b7dce8ad95fb502d23c
William Howard <whoward.tke@gmail.com>
2016-02-20 07:21:21 +0000

    the token is also configurable

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,6 +1,7 @@
 // app/authenticators/oauth2.js
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import ENV from 'ember-twitter/config/environment';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: 'https://ember-twitter-whoward.c9users.io:8082/token'
+  serverTokenEndpoint: `${ENV.apiHost}/token`
 });
